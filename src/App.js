4adc1cf0305ff385ecb0aa443e28d078f3426542
Harmeet Singh <harmeet.singh@farenexus.com>
2024-09-23 17:54:20 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { RouterView } from "./routes/Routes";
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="App">      
-=======
-    <div className="App">
-      
->>>>>>> 30370c34c8c2fb5a6f4820bad3ebc61b30289c15
       <RouterView />
     </div>
   );
--- src/HOC/Outlet.js
+++ src/HOC/Outlet.js
@@ -3,22 +3,12 @@ import { Navigate } from "react-router-dom";
 import NavbarHeader from "../components/Navbar";
 
 export const withLogging = (WrappedComponent) => {
-<<<<<<< HEAD
   const isAuth = () => localStorage.getItem("isLoggedIn");
   return () => {
     return (
       <>
         {isAuth ? <NavbarHeader /> : null}
         {isAuth ? <WrappedComponent /> : <Navigate to="/login" />}
-=======
-    const isAuth = localStorage.getItem("isLoggedIn");
-  return () => {
-    return (
-      <>
-      {isAuth ? <NavbarHeader />:null}
-      {isAuth ? <WrappedComponent /> : <Navigate to="/login" />}
-       
->>>>>>> 30370c34c8c2fb5a6f4820bad3ebc61b30289c15
       </>
     );
   };
