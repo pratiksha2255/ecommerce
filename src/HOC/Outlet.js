f3f7f07c69cb5777fba9e613be19d1c5b54061e7
Harmeet Singh <harmeet.singh@farenexus.com>
2024-09-24 11:15:14 +0530

    consoles removed

--- src/HOC/Outlet.js
+++ src/HOC/Outlet.js
@@ -5,13 +5,8 @@ import NavbarHeader from "../components/Navbar";
 export const withLogging = (WrappedComponent) => {
   return () => {
     const isAuth = () => {
-        console.log('requested', localStorage.getItem("isLoggedIn"))
       return localStorage.getItem("isLoggedIn");
     };
-    useEffect(()=>{
-        console.log('mounted',isAuth())
-        isAuth()
-    },[])
     return (
       <>
         {isAuth() ? <NavbarHeader /> : null}
